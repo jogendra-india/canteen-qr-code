555a32bb74c214bd6dbe8680048167e014f26634
Jogendra <jogiitr@gmail.com>
2024-11-09 16:31:49 +0530

    Refactor QrCodeScanner component to use react-qr-reader and react-webcam

--- src/QrCodeScanner.js
+++ src/QrCodeScanner.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
-import QrScanner from "react-qr-scanner";
+import React, { useState, useCallback } from "react";
+import { QrReader } from "react-qr-reader";
+
+const FACING_MODE_USER = "user";
+const FACING_MODE_ENVIRONMENT = "environment";
 
 const QrCodeScanner = () => {
   const [scanResult, setScanResult] = useState(null);
-  const [isBackCamera, setIsBackCamera] = useState(true);
+  const [facingMode, setFacingMode] = useState(FACING_MODE_ENVIRONMENT);
 
+  // Handle QR Code Scan Result
   const handleScan = (data) => {
     if (data) {
       setScanResult(data.text);
@@ -15,26 +19,31 @@ const QrCodeScanner = () => {
     console.error("QR Code Scan Error: ", err);
   };
 
-  const toggleCamera = () => {
-    setIsBackCamera((prevState) => !prevState);
-  };
+  // Toggle Camera
+  const handleClick = useCallback(() => {
+    setFacingMode((prevState) =>
+      prevState === FACING_MODE_USER
+        ? FACING_MODE_ENVIRONMENT
+        : FACING_MODE_USER
+    );
+  }, []);
 
   return (
     <div style={{ textAlign: "center" }}>
       <h1>QR Code Scanner</h1>
-      <QrScanner
+      <QrReader
         delay={300}
         onError={handleError}
-        onScan={handleScan}
+        onResult={handleScan}
+        constraints={{ facingMode }}
         style={{ width: "600px", margin: "auto" }}
-        facingMode={isBackCamera ? "environment" : "user"} // Switch between back and front cameras
       />
       {scanResult ? (
         <p>Scanned Result: {scanResult}</p>
       ) : (
         <p>Scan a QR code to see the result here</p>
       )}
-      <button onClick={toggleCamera} style={{ marginTop: "20px" }}>
+      <button onClick={handleClick} style={{ marginTop: "20px" }}>
         Switch Camera
       </button>
     </div>
