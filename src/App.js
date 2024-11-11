a2340c14b10b093016198b257f8f954665bba7c6
Jogendra <jogiitr@gmail.com>
2024-11-11 13:04:14 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, NavLink, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, NavLink, Navigate } from 'react-router-dom';
 import QrCodeScanner from './QrCodeScanner';
 import apiCallHelper from './apiCallHelper';
 import UnpaidEmployees from './UnpaidEmployees'; // Import the new component
